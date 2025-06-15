700f732de386025d7f9b50aa71e1f89616213169
ClaudioDev <aleledesma687@gmail.com>
2025-06-15 16:57:38 -0300

    enviar email

--- pages/Contac_Us.py
+++ pages/Contac_Us.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.header("Contac my")
+st.header("Contac Me")
+
+with st.form(key="email_formos"):
+    user_email = st.text_input("Your email address")
+    mesagge = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+    if button:
+        print("Y was pressed!")
