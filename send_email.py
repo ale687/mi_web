c6406d74fa0a233e1e50ef1b06a71f01134ee4e3
ClaudioDev <aleledesma687@gmail.com>
2025-06-15 17:15:05 -0300

    creacion de .env y correccion de seguridad de datos

--- send_email.py
+++ send_email.py
@@ -1,19 +1,26 @@
 import smtplib, ssl
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 host = "smtp.gmail.com"
 port = 465
 
-username = "aleledes.dev@gmail.com"
-password = "rhydnbufvgszrvlt"
+username = os.getenv("GMAIL_ADDRESS")
+password = os.getenv("GMAIL_APP_PASSWORD")
 
-receiver = "aleledes.dev@gmail.com"
+receiver = username
 context = ssl.create_default_context()
 
-message = """\
-Subject:Hi!
-How are you?
-Bye!
-"""
+message =  """\
+From: {}
+To: {}
+Subject: Prueba de envío con Python
+
+Hola! Este correo fue enviado desde un 
+script de Python usando variables de entorno.
+""".format(username, receiver)
 
 with smtplib.SMTP_SSL(host, port, context=context) as server:
     server.login(username, password)
