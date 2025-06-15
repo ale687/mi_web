50475933b218336c22b37ffbac19699adb122a1e
ClaudioDev <aleledesma687@gmail.com>
2025-06-15 18:58:08 -0300

    enviar email a travez del formulario

--- send_email.py
+++ send_email.py
@@ -1,28 +1,17 @@
 import smtplib, ssl
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = os.getenv("GMAIL_ADDRESS")
-password = os.getenv("GMAIL_APP_PASSWORD")
+    username = "aleledes.dev@gmail.com"
+    password = "pgxduvayzvdhuksk"
 
-receiver = username
-context = ssl.create_default_context()
+    receiver = "aleledes.dev@gmail.com"
+    context = ssl.create_default_context()
 
-message =  """\
-From: {}
-To: {}
-Subject: Prueba de envío con Python
-
-Hola! Este correo fue enviado desde un 
-script de Python usando variables de entorno.
-""".format(username, receiver)
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
