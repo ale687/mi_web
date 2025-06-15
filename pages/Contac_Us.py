47a10dddf40877fd2db085801e81ef6fb136adcc
ClaudioDev <aleledesma687@gmail.com>
2025-06-15 19:09:59 -0300

    correccion de errores en contac us

--- pages/Contac_Us.py
+++ pages/Contac_Us.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from send_email import send_email
 
 st.header("Contac Me")
