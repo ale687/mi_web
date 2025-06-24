d266a4f3151902e382b27f8f6941f288a7b1208c
ClaudioDev <aleledesma687@gmail.com>
2025-06-23 21:43:14 -0300

    change order apps

--- Home.py
+++ Home.py
@@ -17,21 +17,22 @@ with col2:
     """
     st.info(content)
     
-col3, empy_col, col4 = st.columns([1.5, 0.5, 1.5])
+#col3, empy_col, col4 = st.columns([1.5, 0.5, 1.5])
+col3, col4 = st.columns([1, 1])
 
 df = pd.read_csv("data.csv", sep=";")
 
 with col3:
-    for index, row in df[:10].iterrows():
+    for index, row in df[:5].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row["url"]})")
 
 
-with col4:
-    for index, row in df[10:].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
+#with col4:
+    #for index, row in df[10:].iterrows():
+        #st.header(row["title"])
+        #st.write(row["description"])
+        #st.image("images/" + row["image"])
+        #st.write(f"[Source Code]({row['url']})")
