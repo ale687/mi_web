49b1ca5de3807050f7b0d1071cf3bf62f4b7b5d7
ClaudioDev <aleledesma687@gmail.com>
2025-06-24 01:08:51 -0300

    add new app

--- Home.py
+++ Home.py
@@ -17,7 +17,6 @@ with col2:
     """
     st.info(content)
     
-#col3, empy_col, col4 = st.columns([1.5, 0.5, 1.5])
 col3, col4 = st.columns([1, 1])
 
 df = pd.read_csv("data.csv", sep=";")
@@ -28,11 +27,3 @@ with col3:
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row["url"]})")
-
-
-#with col4:
-    #for index, row in df[10:].iterrows():
-        #st.header(row["title"])
-        #st.write(row["description"])
-        #st.image("images/" + row["image"])
-        #st.write(f"[Source Code]({row['url']})")
