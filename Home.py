24793b0e42e2242e1f2f50a0cde2a1a78cdd4fe1
ClaudioDev <aleledesma687@gmail.com>
2025-06-26 22:36:20 -0300

    Add Technologies I use

--- Home.py
+++ Home.py
@@ -9,21 +9,44 @@ with col1:
     st.image("images/Photo.png")
     
 with col2:
-    st.title("Alejandro Ledesma")
+    st.title("Claudio Ledesma")
     content = """
-    ¡Hola! Mi nombre es Alejandro y soy estudiante de Ingeniería en Sistemas de Información. 
-    Me apasiona la tecnología, la programación y el desarrollo web. 
-    En esta página vas a encontrar más sobre mí, mis proyectos y cómo contactarme.
+    ¡Hola! Mi nombre es Claudio y soy estudiante de Ingeniería en Sistemas de Información.  
+    Me encuentro en formación constante en el área de desarrollo de software, con especial interés 
+    en la programación con Python, el desarrollo web y la integración de APIs.  
+
+    Esta página tiene como objetivo presentar mis proyectos, habilidades técnicas y 
+    formas de contacto para futuras oportunidades laborales en el ámbito IT.
     """
     st.info(content)
     
-col3, col4 = st.columns([1, 1])
+st.subheader("Tecnologías que utilizo")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.markdown("🐍 **Python**")
+    st.markdown("🗄️ **SQL**")
+    st.markdown("🗃️ **Git / GitHub**")
+    
+with col2:
+    st.markdown("🧱 **HTML básico**")
+    st.markdown("📡 **APIs**")
+    st.markdown("🌐 **Streamlit**")
+
+with col3:
+    st.markdown("📊 **Análisis de datos básico**")
+    st.markdown("🔀 **Automatización de tareas**")
+    st.markdown("🛠️ **Trabajo en equipo y resolución de problemas**")
+    
+col3, col4, col5 = st.columns([0.5, 1, 0.5])
 
 df = pd.read_csv("data.csv", sep=";")
 
-with col3:
+with col4:
     for index, row in df[:5].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row["url"]})")
+
